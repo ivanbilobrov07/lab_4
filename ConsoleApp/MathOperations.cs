a08f321c576197adfeeae7db05a89168ea2c7126
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-12-08 01:19:13 +0200

    init

---
+++ ConsoleApp/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class MathOperations
+    {
+        public event DivisionHandler OnEvent;
+        public delegate void DivisionHandler(int result, EventArgs args);
+
+        public int Addition (int a, int b) {  return a + b; }
+
+        public int Subtraction (int a, int b) { return a - b; }
+
+        public int Multiplication (int a, int b) { return a * b; }
+
+        public int Division (int a, int b) 
+        {
+            if (OnEvent == null) return -1;
+
+            OnEvent.Invoke(a / b, EventArgs.Empty);
+
+            return a / b;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public delegate int CountDelegate(string str, char symbol);
+
+        static void Main(string[] args)
+        {
+            CountDelegate FindNumberOfSymbolUsage = delegate (string str, char symbol)
+            {
+                int counter = 0;
+
+                foreach (char letter in str)
+                {
+                    if (letter == symbol)
+                    {
+                        counter++;
+                    }
+                }
+
+                return counter;
+            };
+
+            int NumberOfZerosInString1 = FindNumberOfSymbolUsage("01231230qwd", '0');
+            Console.WriteLine(NumberOfZerosInString1);
+
+            FindNumberOfSymbolUsage = (string str, char symbol) =>
+            {
+                int counter = 0;
+
+                foreach (char letter in str)
+                {
+                    if (letter == symbol)
+                    {
+                        counter++;
+                    }
+                }
+
+                return counter;
+            };
+
+            int NumberOfZerosInString2 = FindNumberOfSymbolUsage("01231230qwd", '0');
+            Console.WriteLine(NumberOfZerosInString2);
+
+            MathOperations MO = new MathOperations();
+            MO.OnEvent += DivisionHandler;
+
+            MO.Division(4, 1);
+
+
+        }
+        
+        public static void DivisionHandler(int result, EventArgs args)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
